3bbc99bc700e497701d838efd11ea18d0a133f07
SonyaLegis <sonya.legis@gmail.com>
2022-05-03 02:36:35 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_const_constructors, unused_import
+
+import 'package:flutter/material.dart';
+import 'package:sonya_flutter_application/DrawerScreen.dart';
+import 'package:sonya_flutter_application/LibraryHome.dart';
+import 'package:sonya_flutter_application/LoginPage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Library',
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        appBar: AppBar(
+          title: Text('E-Library'),
+        ),
+        body: Stack(
+          children: [
+            DrawerScreen(),
+            LibraryHome(),
+          ],
+        ),
+      ),
+    );
+  }
+}
